93a896fcea49dc64951b021906eb70c4e5e81a0c
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2019-03-10 00:12:08 -0300

    improving category list component template"

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CategorListComponent } from './categor-list/categor-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
-  { path: '', component: CategorListComponent },
+  { path: '', component: CategoryListComponent },
   { path: 'new', component: CategoryFormComponent },
   { path: ':id/edit', component: CategoryFormComponent }
 ];
--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategorListComponent } from './categor-list/categor-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 
 @NgModule({
-  declarations: [CategorListComponent, CategoryFormComponent],
+  declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule
