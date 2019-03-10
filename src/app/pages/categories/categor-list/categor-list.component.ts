48920b858e9f8e323ccacfacca901758f80f2192
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2019-03-09 23:35:16 -0300

    adding categories module

---
+++ src/app/pages/categories/categor-list/categor-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categor-list',
+  templateUrl: './categor-list.component.html',
+  styleUrls: ['./categor-list.component.css']
+})
+export class CategorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
